be33501761e4d0ca9d01c54e512f832273c1d56a
etefy <ahmed.etefy12@gmail.com>
2018-04-04 14:20:18 +0200

    Registered models ZappyUser and Tweet in admin

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Register your models here.
+from django.contrib import admin
+from .models import Tweet
+
+admin.site.register([Tweet])
