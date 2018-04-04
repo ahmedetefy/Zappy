32065d0c2176956e50789fd14f712534c4a63c1c
etefy <ahmed.etefy12@gmail.com>
2018-04-04 13:30:42 +0200

    Added ZappyLogout view in views.py and url in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
 from rest_framework_jwt.views import obtain_jwt_token
-
+from .views import ZappyLogout
 
 urlpatterns = [
     url(r'^token/$', obtain_jwt_token, name="login"),
+    url(r'^logout/$', ZappyLogout.as_view(), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import uuid
+from rest_framework import views, permissions, status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ZappyLogout(views.APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        user.jwt_secret = uuid.uuid4()
+        user.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
