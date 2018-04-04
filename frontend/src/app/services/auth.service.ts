a7674267bc921e2f076e605c704feec81b1df6ec
etefy <ahmed.etefy12@gmail.com>
2018-04-04 17:46:15 +0200

    Added login and logout functions to auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { User } from '../models/user';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class AuthService {
+  private BASE_URL: string = 'http://0.0.0.0:8000/api/jwt';
+  private headers: Headers = new Headers({'Content-Type': 'application/json'});
+  constructor(private http: Http) {}
 
-  constructor() { }
+  login(user: User): Promise<any> {
+    let url: string = `${this.BASE_URL}/token/`;
+    return this.http.post(url, user, {headers: this.headers}).toPromise();
+  }
+
+   logout(token): Promise<any> {
+    let url: string = `${this.BASE_URL}/logout/`;
+    let headers: Headers = new Headers({
+      'Content-Type': 'application/json',
+      Authorization: `JWT ${token}`
+    });
+    return this.http.post(url, {}, {headers: headers}).toPromise();
+   }
 
 }
